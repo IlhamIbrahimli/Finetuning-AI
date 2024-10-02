222014ad48c2580fc13f0a1492da51b1e8a49a08
Ilham Ibrahimli <78649705+IlhamIbrahimli@users.noreply.github.com>
2024-10-02 10:08:33 +0100

    Add files via upload

--- Preproccess/timegrouping.py
+++ Preproccess/timegrouping.py
@@ -1,12 +1,16 @@
+from datetime import datetime
 filename = input("AAA>")
 file = open(filename, "r+")
 
 lines = file.read().splitlines()
 def get_time(message_line):
     dt, msg = message_line.split(" - ")
-    date,time = dt.split(", ")
-    print(date)
-    print(time)
+    user, message = msg.split(":")
+
+    dt_obj = datetime.strptime(dt, "%d/%m/%Y, %I:%M %p")
+    print(dt_obj)
+    return dt_obj, user
+
 reverselines = lines[::-1]
 def merge_multilines(rlines1):
     rlines = rlines1
@@ -16,23 +20,47 @@ def merge_multilines(rlines1):
         count=0
         for i in rlines:
             if " - " not in i:
-                print(i)
+
                 rlines[c+1] += f"\n{i}"
                 rlines.remove(i)
             else:
                 count += 1
             c+=1
-        print(rlines)
-        print(len(rlines))
     return rlines
     
 reverselines = merge_multilines(reverselines)
 
 lines = reverselines[::-1]
+#print(lines)
+for i in range(len(lines)):
+    if "am" in lines[i]:
+        lines[i] = lines[i].replace("am","AM",1)
+    else:
+        lines[i] = lines[i].replace("pm","PM",1)
 print(lines)
-
+## Remove Links and media
 for i in lines:
-    get_time(i)
+
     if "http" in i:
         lines.remove(i)
+    get_time(i)
+for c,i in enumerate(lines):
+
+    if "<This message was edited>" in i:
+        temp = i.split("<")
+        lines[c] = temp[0]
+for c,i in enumerate(lines):
+
+    if "<Media omitted>" in i:
+        temp = i.split("<")
+        lines[c] = temp[0]
+
+## Group by time
+for i in range(len(lines)):
+    time,user = get_time(lines)
+    
+
+
+#print(lines)
+
 
